177fa9ca68e869932b871acba6ebd33fe0f48e4d
许涛 <https://github.com/liubenyin/liu-java.git>
2022-01-14 17:39:30 +0800

    啦啦啦啦啦

--- src/com/sict/test/Helloo_lby.java
+++ src/com/sict/test/Helloo_lby.java
@@ -1,10 +1,20 @@
 package com.sict.test;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Helloo_lby {
     public static void main(String[] args) {
         System.out.println("奉我为主");
+        double d1=3.15423675;
+        double d2=15.203145987;
+        DecimalFormat df = new DecimalFormat("#.000");
+
+        String re1 = df.format(d1);
+        String re2 = df.format(d2);
+        System.out.println(re1);
+        System.out.println(re2);
+
         /*Scanner sc=new Scanner(System.in);
         System.out.println("请输入第一个");
         double a= (double) sc.nextFloat();
