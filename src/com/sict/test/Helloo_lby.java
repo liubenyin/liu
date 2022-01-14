9a7d08afede3e935fa2b6ea1102c43eb72a25c46
许涛 <https://github.com/liubenyin/liu-java.git>
2022-01-14 16:59:15 +0800

    啦啦啦啦啦

--- src/com/sict/test/Helloo_lby.java
+++ src/com/sict/test/Helloo_lby.java
@@ -1,7 +1,24 @@
 package com.sict.test;
 
+import java.util.Scanner;
+
 public class Helloo_lby {
     public static void main(String[] args) {
         System.out.println("奉我为主");
+        /*Scanner sc=new Scanner(System.in);
+        System.out.println("请输入第一个");
+        double a= (double) sc.nextFloat();
+        System.out.println("第二个数");
+        double b=(double) sc.nextFloat();
+        double se=cheng(a,b);*/
+
     }
-}
+    /*public static double cheng(double a,double b) {
+        double d = a * b;
+       // System.out.println(d);
+        System.out.println(d);
+        return d;
+
+    }*/
+
+    }
